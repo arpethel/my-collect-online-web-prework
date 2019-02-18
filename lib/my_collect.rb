d577b7506b5a37defe52b7c921f0893c6162ae9c
Ryan Pethel <arpethel@gmail.com>
2019-02-18 23:05:08 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,21 @@
+def my_collect(array)
+  i = 0
 
+  while i < array.length
+    yield(array[i])
+    i = i + 1
+  end
+  array
+end
 
+# students = ["Tim Jones", "Tom Smith", "Sophie Johnson", "Antoin Miller"]
+#
+# my_collect(students) do |student|
+#   student.split(" ").first
+# end
+
+languages = ["ruby", "javascript", "python", "objective-c"]
+
+my_collect(languages) do |language|
+  language.upcase
+end
